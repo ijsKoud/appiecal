b4eb2f3002ec92fbeeae5527a337fe6faf5367cb
Daan Klarenbeek <github@ijskoud.dev>
2024-10-23 13:30:52 +0200

    chore: initial commit

---
+++ src/main/kotlin/dev/ijskoud/appiecal/AppieCalApplication.kt
@@ -0,0 +1,11 @@
+package dev.ijskoud.appiecal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppieCalApplication
+
+fun main(args: Array<String>) {
+    runApplication<AppieCalApplication>(*args)
+}
