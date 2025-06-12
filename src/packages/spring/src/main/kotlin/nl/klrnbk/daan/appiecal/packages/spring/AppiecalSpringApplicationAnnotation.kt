a79d1951678a8e8d5b081dedbe9ddc7bdaeed624
Daan <daan@klrnbk.nl>
2025-06-12 14:08:17 +0200

    feat: add spring application annotation

---
+++ src/packages/spring/src/main/kotlin/nl/klrnbk/daan/appiecal/packages/spring/AppiecalSpringApplicationAnnotation.kt
@@ -0,0 +1,6 @@
+package nl.klrnbk.daan.appiecal.packages.spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+annotation class AppiecalSpringApplicationAnnotation
