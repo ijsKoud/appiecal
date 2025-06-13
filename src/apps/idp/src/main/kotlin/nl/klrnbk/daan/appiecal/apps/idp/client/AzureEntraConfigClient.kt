beff1e651c9a2b6587b8d692c2d171291329f7c1
Daan <daan@klrnbk.nl>
2025-06-13 15:39:14 +0200

    feat: add azure entra config handlers

---
+++ src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/client/AzureEntraConfigClient.kt
@@ -0,0 +1,11 @@
+package nl.klrnbk.daan.appiecal.apps.idp.client
+
+import nl.klrnbk.daan.appiecal.apps.idp.config.AzureEntraConfig
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@EnableConfigurationProperties(AzureEntraConfig::class)
+class AzureEntraConfigClient(
+    val config: AzureEntraConfig,
+)
