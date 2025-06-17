74b3f272ce475a86caafc9914d0e03dae06a228a
Daan <daan@klrnbk.nl>
2025-06-17 14:29:57 +0200

    feat(CustomAuthenticationEntryPoint): add improved error handling

--- src/packages/security-idp/src/main/kotlin/nl/klrnbk/daan/appiecal/packages/security/idp/config/CustomAuthenticationEntryPoint.kt
+++ src/packages/security-idp/src/main/kotlin/nl/klrnbk/daan/appiecal/packages/security/idp/config/CustomAuthenticationEntryPoint.kt
@@ -1,18 +1,26 @@
 package nl.klrnbk.daan.appiecal.packages.security.idp.config
 
+import com.auth0.jwt.exceptions.AlgorithmMismatchException
+import com.auth0.jwt.exceptions.SignatureVerificationException
+import com.auth0.jwt.exceptions.TokenExpiredException
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import nl.klrnbk.daan.appiecal.packages.exceptions.models.ApiException
 import nl.klrnbk.daan.appiecal.packages.exceptions.responses.error.ErrorResponse
+import nl.klrnbk.daan.appiecal.packages.security.idp.exceptions.InvalidJwtException
+import nl.klrnbk.daan.appiecal.packages.security.idp.exceptions.JwtVerifyException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.AuthenticationServiceException
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
 import java.io.IOException
 import java.io.Serializable
 
+@Component
 class CustomAuthenticationEntryPoint :
     AuthenticationEntryPoint,
     Serializable {
@@ -30,12 +38,22 @@ class CustomAuthenticationEntryPoint :
                 request,
                 response,
                 authException,
-                HttpStatus.UNAUTHORIZED,
+                HttpStatus.FORBIDDEN,
             )
         }
 
         val rootCause: Throwable = authException.cause!!
-        handleErrorResponse(request, response, rootCause, HttpStatus.INTERNAL_SERVER_ERROR)
+        val status =
+            when (rootCause) {
+                is JwtVerifyException -> HttpStatus.UNAUTHORIZED
+                is InvalidJwtException -> HttpStatus.FORBIDDEN
+                is AlgorithmMismatchException -> HttpStatus.UNAUTHORIZED
+                is TokenExpiredException -> HttpStatus.FORBIDDEN
+                is SignatureVerificationException -> HttpStatus.FORBIDDEN
+                else -> HttpStatus.INTERNAL_SERVER_ERROR
+            }
+
+        handleErrorResponse(request, response, rootCause, status)
     }
 
     @Throws(IOException::class)
@@ -50,13 +68,26 @@ class CustomAuthenticationEntryPoint :
         )
 
         val error =
-            ErrorResponse(
-                status = status.value(),
-                type = status.name,
-                message = "Error setting up security context",
-                detail = ex.message ?: "Error setting up security context",
-                instance = request.requestURI,
-            )
+            when (ex) {
+                is ApiException -> {
+                    ErrorResponse(
+                        status = status.value(),
+                        type = status.name,
+                        message = ex.message ?: "Error setting up security context",
+                        detail = ex.detail,
+                        instance = request.requestURI,
+                    )
+                }
+
+                else ->
+                    ErrorResponse(
+                        status = status.value(),
+                        type = status.name,
+                        message = "Error setting up security context",
+                        detail = ex.message ?: "Error setting up security context",
+                        instance = request.requestURI,
+                    )
+            }
 
         response.status = status.value()
         response.contentType = "application/problem+json"
--- src/packages/security-idp/src/main/kotlin/nl/klrnbk/daan/appiecal/packages/security/idp/filters/JwtAuthenticationFilter.kt
+++ src/packages/security-idp/src/main/kotlin/nl/klrnbk/daan/appiecal/packages/security/idp/filters/JwtAuthenticationFilter.kt
@@ -3,10 +3,12 @@ package nl.klrnbk.daan.appiecal.packages.security.idp.filters
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import nl.klrnbk.daan.appiecal.packages.security.idp.config.CustomAuthenticationEntryPoint
 import nl.klrnbk.daan.appiecal.packages.security.idp.exceptions.InvalidJwtException
 import nl.klrnbk.daan.appiecal.packages.security.idp.helpers.resolveJwtFromRequest
 import nl.klrnbk.daan.appiecal.packages.security.idp.models.JwtAuthenticationToken
 import nl.klrnbk.daan.appiecal.packages.security.idp.service.OpenIdService
+import org.springframework.security.authentication.AuthenticationServiceException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository
 import org.springframework.stereotype.Component
@@ -15,6 +17,7 @@ import org.springframework.web.filter.OncePerRequestFilter
 @Component
 class JwtAuthenticationFilter(
     val openIdService: OpenIdService,
+    val authenticationEntryPoint: CustomAuthenticationEntryPoint,
 ) : OncePerRequestFilter() {
     private val securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy()
     private val securityContextRepository = RequestAttributeSecurityContextRepository()
@@ -27,8 +30,18 @@ class JwtAuthenticationFilter(
         val token = resolveJwtFromRequest(request)
         if (token.isNullOrBlank()) return filterChain.doFilter(request, response)
 
-        val decoded = openIdService.decodeAndVerifyJwt(token)
-        if (decoded == null) throw InvalidJwtException()
+        val decoded =
+            try {
+                val decoded = openIdService.decodeAndVerifyJwt(token)
+                if (decoded == null) throw InvalidJwtException()
+
+                decoded
+            } catch (exception: Exception) {
+                val authException = AuthenticationServiceException(exception.message, exception)
+                authenticationEntryPoint.commence(request, response, authException)
+
+                return
+            }
 
         val context = securityContextHolderStrategy.createEmptyContext()
         context.authentication = JwtAuthenticationToken(decoded, openIdService)
