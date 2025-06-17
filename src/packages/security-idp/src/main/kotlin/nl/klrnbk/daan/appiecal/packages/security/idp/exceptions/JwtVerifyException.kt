d607884161c2af43253c24c80a064a9258106f59
Daan <daan@klrnbk.nl>
2025-06-17 13:48:44 +0200

    feat: add exceptions

---
+++ src/packages/security-idp/src/main/kotlin/nl/klrnbk/daan/appiecal/packages/security/idp/exceptions/JwtVerifyException.kt
@@ -0,0 +1,14 @@
+package nl.klrnbk.daan.appiecal.packages.security.idp.exceptions
+
+import nl.klrnbk.daan.appiecal.packages.exceptions.models.ApiException
+import org.springframework.http.HttpStatus
+
+class JwtVerifyException(
+    detail: String,
+    instance: String? = null,
+) : ApiException(
+        HttpStatus.UNAUTHORIZED,
+        "Error while setting up security context",
+        detail,
+        instance,
+    )
