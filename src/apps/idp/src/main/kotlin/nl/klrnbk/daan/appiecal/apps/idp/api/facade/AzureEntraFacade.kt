39326dd69a996bdf4341e460b610115f65442e2e
Daan <daan@klrnbk.nl>
2025-06-18 19:58:46 +0200

    chore: add logging

--- src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/facade/AzureEntraFacade.kt
+++ src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/facade/AzureEntraFacade.kt
@@ -2,6 +2,8 @@ package nl.klrnbk.daan.appiecal.apps.idp.api.facade
 
 import nl.klrnbk.daan.appiecal.apps.idp.api.service.AzureEntraService
 import nl.klrnbk.daan.appiecal.apps.idp.api.service.AzureEntraUserIdpLinkService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,12 +11,16 @@ class AzureEntraFacade(
     private val azureEntraService: AzureEntraService,
     private val azureEntraUserIdpLinkService: AzureEntraUserIdpLinkService,
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(AzureEntraFacade::class.java)
+
     fun getAzureEntraUrl(): String = azureEntraService.getAzureEntraUrl()
 
     fun linkUserWithAzureEntra(
         authorizationCode: String,
         userId: String,
     ) {
+        logger.info("Linking Idp user with Azure Entra credentials; user=$userId")
+
         val token = azureEntraService.authorizeWithCode(authorizationCode)
         azureEntraUserIdpLinkService.createOrReplaceLink(
             userId,
--- src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/facade/TokenFacade.kt
+++ src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/facade/TokenFacade.kt
@@ -2,6 +2,8 @@ package nl.klrnbk.daan.appiecal.apps.idp.api.facade
 
 import nl.klrnbk.daan.appiecal.apps.idp.api.service.AzureEntraService
 import nl.klrnbk.daan.appiecal.apps.idp.api.service.AzureEntraUserIdpLinkService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,14 +11,18 @@ class TokenFacade(
     private val azureEntraUserIdpLinkService: AzureEntraUserIdpLinkService,
     private val azureEntraService: AzureEntraService,
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(TokenFacade::class.java)
+
     fun getAccessToken(userId: String): String? {
         val linkDetails = azureEntraUserIdpLinkService.getLinkFromUserId(userId)
         if (linkDetails == null) return null
 
         if (!linkDetails.isExpired()) return linkDetails.accessToken
-        val updatedToken = azureEntraService.getAccessTokenFromRefreshToken(linkDetails.refreshToken)
+        logger.info("Refreshing access-token for user=$userId; original-expiration-date=${linkDetails.expirationDate}")
 
+        val updatedToken = azureEntraService.getAccessTokenFromRefreshToken(linkDetails.refreshToken)
         azureEntraUserIdpLinkService.createOrReplaceLink(userId, updatedToken)
+
         return updatedToken.accessToken
     }
 }
