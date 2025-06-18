ae7eb274f101ece5518115c15db020666dee6656
Daan <daan@klrnbk.nl>
2025-06-18 20:07:26 +0200

    refactor: EncryptionHelper usage

--- src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/service/AzureEntraUserIdpLinkService.kt
+++ src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/service/AzureEntraUserIdpLinkService.kt
@@ -1,18 +1,23 @@
 package nl.klrnbk.daan.appiecal.apps.idp.api.service
 
 import nl.klrnbk.daan.appiecal.apps.idp.api.models.AzureEntraTokenDetails
+import nl.klrnbk.daan.appiecal.apps.idp.config.IdpEncryptionConfig
 import nl.klrnbk.daan.appiecal.apps.idp.datasource.models.AzureEntraUserIdpLinkModel
 import nl.klrnbk.daan.appiecal.apps.idp.datasource.repositories.AzureEntraUserIdpLinkRepository
 import nl.klrnbk.daan.appiecal.apps.idp.helpers.EncryptionHelper
 import nl.klrnbk.daan.appiecal.packages.common.exceptions.JpaException
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Service
 import kotlin.jvm.optionals.getOrNull
 
 @Service
+@EnableConfigurationProperties(IdpEncryptionConfig::class)
 class AzureEntraUserIdpLinkService(
     private val repository: AzureEntraUserIdpLinkRepository,
-    private val encryptionHelper: EncryptionHelper,
+    encryptionConfig: IdpEncryptionConfig,
 ) {
+    private val encryptionHelper = EncryptionHelper(encryptionConfig)
+
     fun createOrReplaceLink(
         userId: String,
         token: AzureEntraTokenDetails,
