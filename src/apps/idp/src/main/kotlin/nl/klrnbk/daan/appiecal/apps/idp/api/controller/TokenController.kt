d1d244d732e283c48a35d607e85a76cc3af6fc1a
Daan <daan@klrnbk.nl>
2025-06-18 20:23:37 +0200

    docs(TokenController): API documentation

--- src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/controller/TokenController.kt
+++ src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/controller/TokenController.kt
@@ -1,5 +1,10 @@
 package nl.klrnbk.daan.appiecal.apps.idp.api.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import nl.klrnbk.daan.appiecal.apps.idp.api.facade.TokenFacade
 import nl.klrnbk.daan.appiecal.packages.security.idp.models.JwtAuthenticationToken
@@ -15,6 +20,20 @@ class TokenController(
     private val tokenFacade: TokenFacade,
 ) {
     @GetMapping("/access-token")
+    @Operation(
+        summary = "Retrieve an access token to interact with the @AH APIs",
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "(Fresh) usable access token ready to use",
+        content = [
+            Content(
+                mediaType = "text/plain",
+                schema = Schema(implementation = String::class),
+                examples = [ExampleObject(value = "xxx.yyy.zzz")],
+            ),
+        ],
+    )
     @PreAuthorize("@scopes.hasScope(authentication, 'https://klrnbk.nl/projects/appiecal:use')")
     fun getAccessToken(authentication: JwtAuthenticationToken): String? = tokenFacade.getAccessToken(authentication.principal)
 }
