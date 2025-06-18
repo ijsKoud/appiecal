a398dffb96d9c148d65413fa2f07d1413aa05a48
Daan <daan@klrnbk.nl>
2025-06-18 16:32:08 +0200

    feat: return new token when expired

--- src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/facade/TokenFacade.kt
+++ src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/api/facade/TokenFacade.kt
@@ -1,14 +1,22 @@
 package nl.klrnbk.daan.appiecal.apps.idp.api.facade
 
+import nl.klrnbk.daan.appiecal.apps.idp.api.service.AzureEntraService
 import nl.klrnbk.daan.appiecal.apps.idp.api.service.AzureEntraUserIdpLinkService
 import org.springframework.stereotype.Service
 
 @Service
 class TokenFacade(
     private val azureEntraUserIdpLinkService: AzureEntraUserIdpLinkService,
+    private val azureEntraService: AzureEntraService,
 ) {
     fun getAccessToken(userId: String): String? {
         val linkDetails = azureEntraUserIdpLinkService.getLinkFromUserId(userId)
-        return linkDetails?.accessToken
+        if (linkDetails == null) return null
+
+        if (!linkDetails.isExpired()) return linkDetails.accessToken
+        val updatedToken = azureEntraService.getAccessTokenFromRefreshToken(linkDetails.refreshToken)
+
+        azureEntraUserIdpLinkService.createOrReplaceLink(userId, updatedToken)
+        return updatedToken.accessToken
     }
 }
