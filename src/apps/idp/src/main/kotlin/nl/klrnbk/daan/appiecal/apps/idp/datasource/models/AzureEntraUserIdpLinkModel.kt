15640399567c9cd660de71d30bbd5135536e06db
Daan <daan@klrnbk.nl>
2025-06-18 16:13:24 +0200

    fix: table not defined

--- src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/datasource/models/AzureEntraUserIdpLinkModel.kt
+++ src/apps/idp/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/idp/datasource/models/AzureEntraUserIdpLinkModel.kt
@@ -3,9 +3,11 @@ package nl.klrnbk.daan.appiecal.apps.idp.datasource.models
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.time.LocalDateTime
 
 @Entity
+@Table(name = "azure_entra_user_idp_link")
 class AzureEntraUserIdpLinkModel(
     @Id val id: String,
     @Column(name = "expiration_date") val expirationDate: LocalDateTime,
