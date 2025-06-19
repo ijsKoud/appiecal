fa686340a73e2671c5f97673d235f5f13f7ec248
Daan <daan@klrnbk.nl>
2025-06-19 10:20:07 +0200

    feat: add more fields to JwtAuthenticationToken

--- src/packages/security-idp/src/main/kotlin/nl/klrnbk/daan/appiecal/packages/security/idp/constants/JwtContants.kt
+++ src/packages/security-idp/src/main/kotlin/nl/klrnbk/daan/appiecal/packages/security/idp/constants/JwtContants.kt
@@ -5,3 +5,5 @@ const val TOKEN_HEADER_VALUE_PREFIX = "Bearer "
 
 const val AUTHORITY_GROUP_PREFIX = "group:"
 const val AUTHORITY_SCOPE_PREFIX = "scope:"
+
+const val SERVICE_ACCOUNT_SCOPE = "goauthentik.io/service-account"
