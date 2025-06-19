41f1628fc0a36a3c50209eb29485e52d5f57d4e4
Daan <daan@klrnbk.nl>
2025-06-19 14:31:47 +0200

    feat(GqlClient): add query implementations

--- src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/clients/gql/GqlClient.kt
+++ src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/clients/gql/GqlClient.kt
@@ -1,5 +1,9 @@
 package nl.klrnbk.daan.appiecal.apps.schedule.clients.gql
 
+import nl.klrnbk.daan.appiecal.apps.schedule.clients.gql.models.activity.GqlActivityBody
+import nl.klrnbk.daan.appiecal.apps.schedule.clients.gql.models.activity.GqlActivityResponse
+import nl.klrnbk.daan.appiecal.apps.schedule.clients.gql.models.schedule.GqlScheduleBody
+import nl.klrnbk.daan.appiecal.apps.schedule.clients.gql.models.schedule.GqlScheduleResponse
 import nl.klrnbk.daan.appiecal.packages.common.retrofit.RetrofitClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
@@ -9,4 +13,22 @@ class GqlClient(
     @Value("\${appiecal.schedule.gql.url}") baseUrl: String,
 ) : RetrofitClient() {
     private val apiClient = getRetrofitClient(GqlApiInterface::class.java, baseUrl)
+
+    fun getSchedule(
+        token: String,
+        startDate: String,
+        endDate: String,
+    ): GqlScheduleResponse {
+        val call = apiClient.getSchedule("Bearer $token", GqlScheduleBody(startDate, endDate))
+        return handleApiCall(call)
+    }
+
+    fun getScheduleActivities(
+        token: String,
+        startDate: String,
+        endDate: String,
+    ): GqlActivityResponse {
+        val call = apiClient.getActivities("Bearer $token", GqlActivityBody(startDate, endDate))
+        return handleApiCall(call)
+    }
 }
