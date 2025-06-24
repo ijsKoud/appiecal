d9303f233438d46794dfc1584649015d85f3c52a
Daan <daan@klrnbk.nl>
2025-06-24 23:25:29 +0200

    feat: add response models

---
+++ src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/api/responses/schedule/ScheduleActivity.kt
@@ -0,0 +1,42 @@
+package nl.klrnbk.daan.appiecal.apps.schedule.api.responses.schedule
+
+import nl.klrnbk.daan.appiecal.apps.schedule.clients.gql.models.activity.GqlActivityResponseActivity
+import nl.klrnbk.daan.appiecal.apps.schedule.constants.DATE_TIME_FORMATTER
+import nl.klrnbk.daan.appiecal.apps.schedule.constants.ShiftDepartment
+import nl.klrnbk.daan.appiecal.apps.schedule.datasource.models.ActivityModel
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class ScheduleActivity(
+    val id: UUID,
+    val description: String,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+    val department: ShiftDepartment,
+    val timeCode: String,
+    val paid: Boolean,
+) {
+    companion object {
+        fun fromModel(model: ActivityModel): ScheduleActivity =
+            ScheduleActivity(
+                id = UUID.fromString(model.id),
+                description = model.description,
+                startDate = model.startDate,
+                endDate = model.endDate,
+                department = model.department,
+                timeCode = model.timeCode,
+                paid = model.paid,
+            )
+
+        fun fromGqlResponse(response: GqlActivityResponseActivity): ScheduleActivity =
+            ScheduleActivity(
+                id = UUID.randomUUID(),
+                description = response.description,
+                startDate = LocalDateTime.parse(response.startTime, DATE_TIME_FORMATTER),
+                endDate = LocalDateTime.parse(response.endTime, DATE_TIME_FORMATTER),
+                department = ShiftDepartment.getFromGqlResponse(response.teamName),
+                timeCode = response.timeCode,
+                paid = !response.unpaid,
+            )
+    }
+}
---
+++ src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/api/responses/schedule/ScheduleResponse.kt
@@ -0,0 +1,37 @@
+package nl.klrnbk.daan.appiecal.apps.schedule.api.responses.schedule
+
+data class ScheduleResponse(
+    val shifts: List<ScheduleShift>,
+) {
+    val amount: Int = shifts.size
+
+    companion object {
+        const val SCHEDULE_RESPONSE_EXAMPLE = """
+            {
+              "amount": 1,
+              "shifts": [
+                {
+                  "id": "fcb42be0-7988-4ae8-83af-1d453b0a423e",
+                  "storeId": "1148",
+                  "startDate": "2025-05-18T20:00:00",
+                  "endDate": "2025-05-18T23:00:00",
+                  "departments": [
+                    "Operatie"
+                  ],
+                  "activities": [
+                    {
+                      "id": "44a275cb-fb32-41c1-ad22-2c522028e6bc",
+                      "description": "Promotie",
+                      "startDate": "2025-05-18T20:00:00",
+                      "endDate": "2025-05-18T23:00:00",
+                      "department": "Operatie",
+                      "timeCode": "WRK",
+                      "paid": true
+                    }
+                  ]
+                }
+              ]
+            }
+        """
+    }
+}
