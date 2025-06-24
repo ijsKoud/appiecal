ca32e69cb9483d05e0afe6e1b951d47f7549c20a
Daan <daan@klrnbk.nl>
2025-06-25 00:04:53 +0200

    fix: endpoint is GET and not POST

--- src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/clients/idp/IdpApiInterface.kt
+++ src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/clients/idp/IdpApiInterface.kt
@@ -1,12 +1,12 @@
 package nl.klrnbk.daan.appiecal.apps.schedule.clients.idp
 
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface IdpApiInterface {
-    @POST("/v1/token/internal/access-token/{userId}")
+    @GET("/v1/token/internal/access-token/{userId}")
     fun getAccessToken(
         @Header("X-Authorization") token: String,
         @Path("userId") userId: String,
