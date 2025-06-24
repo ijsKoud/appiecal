de7863bdfd1e177cac59132c5ecb5c5e1af61cc9
Daan <daan@klrnbk.nl>
2025-06-24 23:25:45 +0200

    feat: add fetch formatted schedule endpoint

--- src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/api/facade/RawRequestFacade.kt
+++ src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/api/facade/RawRequestFacade.kt
@@ -1,9 +1,13 @@
 package nl.klrnbk.daan.appiecal.apps.schedule.api.facade
 
+import nl.klrnbk.daan.appiecal.apps.schedule.api.responses.schedule.ScheduleResponse
 import nl.klrnbk.daan.appiecal.apps.schedule.api.service.GqlService
 import nl.klrnbk.daan.appiecal.apps.schedule.clients.gql.models.schedule.GqlScheduleResponseSchedule
+import nl.klrnbk.daan.appiecal.apps.schedule.constants.DATE_TIME_FORMATTER
+import nl.klrnbk.daan.appiecal.apps.schedule.helpers.getUniqueStoreIds
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
+import kotlin.text.format
 
 @Service
 class RawRequestFacade(
@@ -13,5 +17,34 @@ class RawRequestFacade(
         token: String,
         startDate: LocalDateTime,
         endDate: LocalDateTime,
-    ): List<GqlScheduleResponseSchedule> = gqlService.getRawSchedule(token, startDate, endDate)
+    ): List<GqlScheduleResponseSchedule> {
+        val startDateString = startDate.format(DATE_TIME_FORMATTER)
+        val endDateString = endDate.format(DATE_TIME_FORMATTER)
+
+        return gqlService.getRawSchedule(token, startDateString, endDateString)
+    }
+
+    fun getFormattedSchedule(
+        token: String,
+        startDate: LocalDateTime,
+        endDate: LocalDateTime,
+    ): ScheduleResponse {
+        val startDateString = startDate.format(DATE_TIME_FORMATTER)
+        val endDateString = endDate.format(DATE_TIME_FORMATTER)
+
+        val shifts = gqlService.getFetchedShifts(token, startDateString, endDateString)
+        val activities =
+            getUniqueStoreIds(shifts)
+                .map {
+                    gqlService.getFetchedActivities(
+                        token,
+                        it,
+                        startDateString,
+                        endDateString,
+                    )
+                }.flatten()
+
+        val mergedShifts = gqlService.mergeActivitiesWithShifts(shifts, activities)
+        return ScheduleResponse(mergedShifts)
+    }
 }
