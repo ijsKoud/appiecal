33e08bac37325e58a706de12c431c6cf4c40cb91
Daan <daan@klrnbk.nl>
2025-06-26 00:33:50 +0200

    feat: add date-time transformers

--- src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/constants/DateTimeConstants.kt
+++ src/apps/schedule/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/schedule/constants/DateTimeConstants.kt
@@ -1,6 +1,10 @@
 package nl.klrnbk.daan.appiecal.apps.schedule.constants
 
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 const val DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss"
+const val DATE_TIME_ZONE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssX"
+val AH_TIME_ZONE: ZoneId = ZoneId.of("Europe/Amsterdam")
+
 val DATE_TIME_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)
