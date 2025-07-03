8e73afaccc1ad5acc87686137f9e673eae293592
Daan <daan@klrnbk.nl>
2025-07-03 15:03:27 +0200

    feat: add methods to link user to caldav credentials

---
+++ src/apps/calendar/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/calendar/api/service/CalendarCredentialsService.kt
@@ -0,0 +1,37 @@
+package nl.klrnbk.daan.appiecal.apps.calendar.api.service
+
+import nl.klrnbk.daan.appiecal.apps.calendar.datasource.models.CalendarCredentialsModel
+import nl.klrnbk.daan.appiecal.apps.calendar.datasource.repositories.CalendarCredentialsRepository
+import nl.klrnbk.daan.appiecal.packages.security.idp.helpers.EncryptionHelper
+import org.springframework.stereotype.Service
+
+@Service
+class CalendarCredentialsService(
+    private val calendarCredentialsRepository: CalendarCredentialsRepository,
+    private val encryptionHelper: EncryptionHelper,
+) {
+    fun saveCredentials(
+        userId: String,
+        authScope: String,
+        authToken: String,
+        baseUrl: String,
+        principalUrl: String,
+        calendarHomeSetUrl: String,
+        calendarUrl: String?,
+    ): CalendarCredentialsModel {
+        val encryptedAuthToken = encryptionHelper.encryptStr(authToken)
+        val model =
+            CalendarCredentialsModel(
+                null,
+                userId,
+                authScope,
+                encryptedAuthToken,
+                baseUrl,
+                principalUrl,
+                calendarHomeSetUrl,
+                calendarUrl,
+            )
+
+        return calendarCredentialsRepository.save(model)
+    }
+}
