3fed28601c08fc9b03cdd27a95390a854a238a61
Daan <daan@klrnbk.nl>
2025-07-27 21:22:08 +0200

    feat: add event controller with delete event endpoint

--- src/apps/calendar/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/calendar/api/service/CaldavService.kt
+++ src/apps/calendar/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/calendar/api/service/CaldavService.kt
@@ -2,6 +2,7 @@ package nl.klrnbk.daan.appiecal.apps.calendar.api.service
 
 import nl.klrnbk.daan.appiecal.apps.calendar.api.models.CalendarListEntryResponse
 import nl.klrnbk.daan.appiecal.apps.calendar.clients.caldav.CaldavClient
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.springframework.stereotype.Service
 
 @Service
@@ -27,5 +28,22 @@ class CaldavService(
     ): List<CalendarListEntryResponse> {
         val response = caldavClient.getCalendarList(url, authScope, authToken)
         return response.map { CalendarListEntryResponse(it.first, it.second) }
+    }
+
+    fun deleteEvent(
+        url: String,
+        authScope: String,
+        authToken: String,
+        eventId: String,
+    ) {
+        val deleteUrl =
+            url
+                .toHttpUrl()
+                .newBuilder()
+                .addPathSegment("$eventId.ics")
+                .build()
+                .toString()
+        println(deleteUrl)
+        caldavClient.deleteEvent(deleteUrl, authScope, authToken)
     }
 }
