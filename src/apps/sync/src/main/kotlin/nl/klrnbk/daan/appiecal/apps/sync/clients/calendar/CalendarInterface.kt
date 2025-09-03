e21a26b76da8eee24d0797c3dbe9066c3de2b2c2
Daan <daan@klrnbk.nl>
2025-09-03 11:57:13 +0200

    fix: build issues

--- src/apps/sync/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/sync/clients/calendar/CalendarInterface.kt
+++ src/apps/sync/src/main/kotlin/nl/klrnbk/daan/appiecal/apps/sync/clients/calendar/CalendarInterface.kt
@@ -3,6 +3,7 @@ package nl.klrnbk.daan.appiecal.apps.sync.clients.calendar
 import nl.klrnbk.daan.appiecal.packages.common.shared.services.calendar.models.CreateEventRequestBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -10,7 +11,7 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CalendarInterface {
-    @POST("/v1/events/{userId}/delete")
+    @DELETE("/v1/events/{userId}/delete")
     fun deleteEvent(
         @Header("x-authorization") authorization: String,
         @Path("userId") userId: String,
